cbbbd67cdc5452032fcbbdd48213d2b5d5c76978
Krzee <derekhunziker@gmail.com>
2023-08-26 14:01:58 -0400

    new

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div className="text-yellow-200">Dashboard</div>;
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-blue-600 pb-4">Welcome to your page, John!</h1>
+            <div className="flex gap-4">
+                <button className="border-white border-4 p-6">yo</button>
+                <button className="border-white border-4 p-6">hello</button>
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
