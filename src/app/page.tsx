e260d705c0c43e9d34ec147485968c457f7872af
Ali <mailme.ali2004@gmail.com>
2023-08-26 14:23:24 -0400

    refactoring page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 import Image from "next/image";
 import Friends from "@/Components/Friends/Friends";
+import Dashboard from "@/Components/Dashboard/Dashboard";
+import Landing from "@/Components/Landing/Landing";
 
 export default function Home() {
   return (
     <div>
+      <Landing />
+      <Dashboard />
       <Friends />
     </div>
   );
