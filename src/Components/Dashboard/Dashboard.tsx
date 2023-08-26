ae03fab929d974663728f3a0b9222fb6b1e2c631
Abrar <abrarmurad00@gmail.com>
2023-08-26 15:34:56 -0400

    hobbies added

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Dashboard = () => {
     return (
         <div className="flex flex-col justify-center items-center bg-slate-900">
-            <h1 className="text-white pb-4 mt-8">Welcome to your page, John!</h1>
+            <h1 className="text-white pb-4 mt-8">
+                Welcome to your page, John!
+            </h1>
             <div className="flex gap-4 pb-4 font-bold">
                 <button className="border-white border-4 p-6 hover:bg-pink-500">
                     Instagram
@@ -30,18 +32,25 @@ const Dashboard = () => {
                 <h1 className="font-bold mb-2">About Me:</h1>
                 <p className="">
                     Hello guys, my name is John! I'm a 2nd year student at
-                    Universi
+                    Wilfrid Laurier University, studying Computer Science with a
+                    minor in finance. I recently went on a trip with my friends
+                    to California! We visisted the Metagooglzon headquarters.
                 </p>
             </div>
             <div className="flex flex-col items-center justify-center p-2 w-[400px]">
                 <h1 className="font-bold mb-2">Current Activity:</h1>
                 <p className="">
-                    🗺️ frequented locations: <br />
-                    🍲 favorite food: <br />
-                    ⚽ favorite sport: <br />
-                    🎨 favorite color: <br />
-                    💼 university / work: <br />
-                    🪀 hobbies: <br />
+                    🗺️ frequented locations: California
+                    <br />
+                    🍲 favorite food: Boba tea
+                    <br />
+                    ⚽ favorite sport: badminton
+                    <br />
+                    🎨 favorite color: green
+                    <br />
+                    💼 university / work: WLU / intern @ <br />
+                    🪀 hobbies: hiking, cooking, and woodworking
+                    <br />
                 </p>
             </div>
         </div>
