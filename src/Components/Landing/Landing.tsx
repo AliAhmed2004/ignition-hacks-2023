aa27df9fecef6028943b3873e8ddf79d64604909
Krzee <derekhunziker@gmail.com>
2023-08-26 14:52:48 -0400

    padding

--- src/Components/Landing/Landing.tsx
+++ src/Components/Landing/Landing.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Landing = () => {
   return (
     <div className="mx-auto flex items-center justify-center flex-col">
-      <div className="h-20 w-full bg-slate-900">yo</div>
+      <div className="h-36 w-full">TITLE</div>
       <div className="mx-auto w-full">
-        <div className="flex gap-0">
+        <div className="flex gap-0 px-36">
           <div className="h-16 w-1/4 bg-blue-600 p-1">
             <button className="flex w-full h-full items-center justify-center bg-gray-700 transition-colors duration-300 ease-in-out hover:bg-gray-800 ">
               <h1 className="text-2xl font-black text-white">Profile</h1>
